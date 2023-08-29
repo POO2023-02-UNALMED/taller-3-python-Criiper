2762799ec0bd146b756246f4cbed8b14ed2059bc
Criiper <perezloperacristiandavid@gmail.com>
2023-08-29 11:11:52 -0500

    Commit 8 :smiley:
    Coloqué un solo __init__ que es el que tiene marca y estado, inicializa los valores, por lo que control inicia en 0.

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,20 +1,13 @@
 class TV:
     numTV = 0
 
-    def __init__(self, marca, estado, control):
+    def __init__(self, marca, estado):
         self.marca = marca
         self.canal = 1
         self.precio = 500
         self.estado = estado
         self.volumen = 1
-        self.control = control
-        
-
-    def __init__(self, marca, estado):
-        self.marca = marca
-        self.estado = estado
-
-        numTV = numTV+1
+        self.control = 0
 
     def turnOn(self):
         self.estado = True
